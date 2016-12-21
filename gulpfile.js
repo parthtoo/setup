ce358951f9ed0189d0c218d2d3f3c95a759de2a1
Parth Mehta <pcm.seven@gmail.com>
2016-12-21 16:02:27 +0530

    gulpfile modified, added ts support

--- gulpfile.js
+++ gulpfile.js
@@ -8,35 +8,36 @@ var connect = require('gulp-connect');
 var fs = require("fs");
 
 var sass = require('gulp-sass');
-var browserify = require("browserify");
-var babelify = require("babelify");
+var browserify = require('browserify');
+var tsify = require('tsify');
+var babelify = require('babelify');
 
-// html, css, js
+// html, scss, ts
 
 gulp.task('html', function() {
 	gulp.src('src/**/*.html')
 	.pipe(gulp.dest('dist'));
 });
 
-gulp.task('css', function() {
-	gulp.src('src/css/**/*.scss')
+gulp.task('scss', function() {
+	gulp.src('src/scss/**/*.scss')
 	.pipe(sass())
 	.pipe(concat('styles.css'))
 	.pipe(gulp.dest('dist'));
 });
 
-gulp.task('js', function() {
-	browserify('src/js/main.js')
-	.transform(babelify, {
-		presets: ['es2015']
-	})
+gulp.task('ts', function() {
+	browserify('src/ts/main.ts')
+	.plugin(tsify, {target: 'es2015'})
+	.transform(babelify, {presets: 'es2015', extensions: '.ts'})
 	.bundle()
+	.on('error', function(err) { console.error(err.toString()); })
 	.pipe(fs.createWriteStream("dist/bundle.js"));
 });
 
 // build and clean
 
-gulp.task('build', ['html', 'css', 'js']);
+gulp.task('build', ['html', 'scss', 'ts']);
 
 gulp.task('clean', function() {
 	gulp.src(['dist/**/*.html', 'dist/styles.css', 'dist/bundle.js'], {
@@ -49,8 +50,8 @@ gulp.task('clean', function() {
 
 gulp.task('serve', function() {
 	gulp.watch('src/**/*.html', ['html']);
-	gulp.watch('src/css/**/*.scss', ['css']);
-	gulp.watch('src/js/**/*.js', ['js']);
+	gulp.watch('src/scss/**/*.scss', ['scss']);
+	gulp.watch('src/ts/**/*.ts', ['ts']);
 	connect.server({
 		root: 'dist',
 	});
