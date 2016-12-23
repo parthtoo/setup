ce641cfe39183f8af90d6f7cd688c2edd598ae1d
Parth Mehta <pcm.seven@gmail.com>
2016-12-23 11:04:43 +0530

    modified gulpfile, only scss and ts

--- gulpfile.js
+++ gulpfile.js
@@ -12,22 +12,17 @@ var browserify = require('browserify');
 var tsify = require('tsify');
 var babelify = require('babelify');
 
-// html, scss, ts
-
-gulp.task('html', function() {
-	gulp.src('src/**/*.html')
-	.pipe(gulp.dest('dist'));
-});
+// scss and ts
 
 gulp.task('scss', function() {
-	gulp.src('src/scss/**/*.scss')
+	gulp.src('scss/**/*.scss')
 	.pipe(sass())
 	.pipe(concat('styles.css'))
 	.pipe(gulp.dest('dist'));
 });
 
 gulp.task('ts', function() {
-	browserify('src/ts/main.ts')
+	browserify('ts/main.ts')
 	.plugin(tsify, {target: 'es2015'})
 	.transform(babelify, {presets: 'es2015', extensions: '.ts'})
 	.bundle()
@@ -37,21 +32,18 @@ gulp.task('ts', function() {
 
 // build and clean
 
-gulp.task('build', ['html', 'scss', 'ts']);
+gulp.task('build', ['scss', 'ts']);
 
 gulp.task('clean', function() {
-	gulp.src(['dist/**/*.html', 'dist/styles.css', 'dist/bundle.js'], {
-		read: false
-	})
+	gulp.src(['dist/styles.css', 'dist/bundle.js'], {read: false})
 	.pipe(clean());
 });
 
 // serve
 
 gulp.task('serve', function() {
-	gulp.watch('src/**/*.html', ['html']);
-	gulp.watch('src/scss/**/*.scss', ['scss']);
-	gulp.watch('src/ts/**/*.ts', ['ts']);
+	gulp.watch('scss/**/*.scss', ['scss']);
+	gulp.watch('ts/**/*.ts', ['ts']);
 	connect.server({
 		root: 'dist',
 	});
