f0a9b543262fc0299fa8489792cc8c7ddf468034
Parth Mehta <pcm.seven@gmail.com>
2017-11-19 22:14:02 +0530

    updated gulpfile to be that of folder FRONTEND

--- gulpfile.js
+++ gulpfile.js
@@ -12,19 +12,25 @@ var browserify = require('browserify');
 var tsify = require('tsify');
 var babelify = require('babelify');
 
-// scss and ts
+// html, scss, ts
+
+gulp.task('html', function() {
+	gulp.src('src/**/*.html')
+	.pipe(gulp.dest('dist'));
+});
 
 gulp.task('scss', function() {
-	gulp.src('scss/**/*.scss')
+	gulp.src('src/scss/**/*.scss')
 	.pipe(sass())
+	.on('error', function(err) { console.error(err.toString()); })
 	.pipe(concat('styles.css'))
 	.pipe(gulp.dest('dist'));
 });
 
 gulp.task('ts', function() {
-	browserify('ts/main.ts')
+	browserify('src/ts/main.ts')
 	.plugin(tsify, {target: 'es2015'})
-	.transform(babelify, {presets: 'es2015', extensions: '.ts'})
+	.transform(babelify, {presets: 'env', extensions: '.ts'})
 	.bundle()
 	.on('error', function(err) { console.error(err.toString()); })
 	.pipe(fs.createWriteStream("dist/bundle.js"));
@@ -32,18 +38,21 @@ gulp.task('ts', function() {
 
 // build and clean
 
-gulp.task('build', ['scss', 'ts']);
+gulp.task('build', ['html', 'scss', 'ts']);
 
 gulp.task('clean', function() {
-	gulp.src(['dist/styles.css', 'dist/bundle.js'], {read: false})
+	gulp.src(['dist/**/*.html', 'dist/styles.css', 'dist/bundle.js'], {
+		read: false
+	})
 	.pipe(clean());
 });
 
 // serve
 
 gulp.task('serve', function() {
-	gulp.watch('scss/**/*.scss', ['scss']);
-	gulp.watch('ts/**/*.ts', ['ts']);
+	gulp.watch('src/**/*.html', ['html']);
+	gulp.watch('src/scss/**/*.scss', ['scss']);
+	gulp.watch('src/ts/**/*.ts', ['ts']);
 	connect.server({
 		root: 'dist',
 	});
